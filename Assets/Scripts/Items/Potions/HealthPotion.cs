05a6f0ba33db247c886714f8512237bce9ef669c
Enrico Ciciriello <21175992+Hacryn@users.noreply.github.com>
2022-08-05 16:37:47 +0200

    Added Green Potion check

--- Assets/Scripts/Items/Potions/HealthPotion.cs
+++ Assets/Scripts/Items/Potions/HealthPotion.cs
@@ -8,7 +8,7 @@ public class HealthPotion : PickUpItem
 
     public override bool OnPickUp(GameObject player) 
     {
-        if (player.GetComponent<PlayerCombat>().HasMaxHealth) return false;
+        if (player.GetComponent<PlayerCombat>().HasMaxHealth && healingAmount > 0) return false;
         player.GetComponent<PlayerCombat>().Health += healingAmount;
         return true;
     }
