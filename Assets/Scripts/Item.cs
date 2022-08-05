01f0fc39abb87b20f1383105c1cbe0c836fd0127
Giovanni Palmieri <gianni00palmieri@gmail.com>
2022-08-05 12:24:38 +0200

    Red potion example

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -24,6 +24,7 @@ public class Item : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             //Do something
-        }   
+            Destroy(gameObject);
+        }
     }
 }
