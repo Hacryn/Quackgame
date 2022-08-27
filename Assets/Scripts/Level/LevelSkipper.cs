bb7251c467d18eb12a62de5e24e9a99a7e769f42
Enrico Ciciriello <21175992+Hacryn@users.noreply.github.com>
2022-08-27 19:20:38 +0200

    Setting up 0B

--- Assets/Scripts/Level/LevelSkipper.cs
+++ Assets/Scripts/Level/LevelSkipper.cs
@@ -8,10 +8,18 @@ public class LevelSkipper : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.F1)) {
-            SceneManager.LoadScene("1A", LoadSceneMode.Single);
+            SceneManager.LoadScene("0A", LoadSceneMode.Single);
         }
 
         if (Input.GetKey(KeyCode.F2)) {
+            SceneManager.LoadScene("0B", LoadSceneMode.Single);
+        }
+
+        if (Input.GetKey(KeyCode.F3)) {
+            SceneManager.LoadScene("1A", LoadSceneMode.Single);
+        }
+
+        if (Input.GetKey(KeyCode.F4)) {
             SceneManager.LoadScene("1B", LoadSceneMode.Single);
         }
     }
